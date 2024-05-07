e0c7bb4f51b19534b6e1a7824e7f0ae4a34c4da4
Matheus Eleutério <100217154+matheus-eleuterio@users.noreply.github.com>
2024-05-07 22:55:38 +0000

    Criação do arquivo de cabeçalho

---
+++ tarefas.h
@@ -0,0 +1,14 @@
+#ifndef TAREFAS_H
+#define TAREFAS_H
+
+void nova_conta();
+void listar_clientes();
+void apagar_cliente();
+void debitar();
+void depositar();
+void extrato();
+void transferencia();
+void salvar();
+void carregar();
+
+#endif
