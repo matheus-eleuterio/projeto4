bf8c98ef8b7d661fe7f8dfbc9f678ef6aabfc732
Matheus Eleutério <100217154+matheus-eleuterio@users.noreply.github.com>
2024-05-07 23:13:42 +0000

    Implementação do Struct e Enum

--- tarefas.h
+++ tarefas.h
@@ -1,6 +1,29 @@
 #ifndef TAREFAS_H
 #define TAREFAS_H
 
+#define MAX_CLIENTES 1000
+#define MAX_OPERACOES 100
+
+typedef enum { COMUM, PLUS } tipo_conta;
+
+typedef struct {
+    char nome[40];
+    char cpf[11]; 
+    tipo_conta tipo;
+    double saldo;
+    char senha[10];
+} Cliente;
+
+typedef struct {
+    char cpf[11];
+    double valor;
+} Operacao;
+
+Cliente clientes[MAX_CLIENTES];
+Operacao operacoes[MAX_OPERACOES];
+int numero_clientes = 0;
+int numero_operacoes = 0;
+
 void nova_conta();
 void listar_clientes();
 void apagar_cliente();
